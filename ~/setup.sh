b3988e0b804fb88aec3e879075ded9e850b5e520
Yanglu Piao <yp2419@columbia.edu>
2023-01-07 11:49:14 -0800

    update setup.sh to zsh

--- ~/setup.sh
+++ ~/setup.sh
@@ -15,20 +15,26 @@ mv ${HOME}/.vimrc ${HOME}/.vimrc.bak
 cp vimrc ${HOME}/.vimrc
 # git
 # apt-get install git
-if grep -q "glocal" ${HOME}/.bashrc
+if grep -q "glocal" ${HOME}/.zshrc
 then
-  echo "skip modifying bashrc"
+  echo "skip modifying zshrc"
 else
   echo "
+autoload -Uz vcs_info
+precmd() { vcs_info }
+zstyle ':vcs_info:git:*' formats '%b '
+setopt PROMPT_SUBST
+PROMPT='%F{green}%*%f %F{blue}%~%f %F{red}\${vcs_info_msg_0_}%f$ '
 # git
 alias glocal='git co master && git pull && git co local && git rebase master'
 alias gilocal='git co master && git pull && git co local && git rebase -i master'
 alias gpush='git push origin local:master'
+alias gs='git status -u .'
 # other
 alias lt='ls -lrt'
-" >> ${HOME}/.bashrc
-  # export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[\033[01;32m\]`git branch 2>/dev/null | grep "^*" | colrm 1 2`\[\033[00m\] \$ '
-  source ${HOME}/.bashrc
+alias zrc='vi ~/.zshrc && source ~/.zshrc'
+" >> ${HOME}/.zshrc
+  source ${HOME}/.zshrc
 fi
 git config --global alias.co checkout
 git config --global alias.st status
